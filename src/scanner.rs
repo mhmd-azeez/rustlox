14e1cd490237f193943180b5bff4ae5af954a0fc
Muhammad Azeez <muhammad-azeez@outlook.com>
2023-07-02 20:18:28 +0300

    scanning on demand, but has a bug

--- src/scanner.rs
+++ src/scanner.rs
@@ -119,9 +119,10 @@ impl<'a> Scanner<'a> {
                 } else {
                     self.make_token(TokenType::Greater)
                 }
-            },
+            }
             '"' => self.make_string(),
-            d if self.is_digit(c) => self.make_number(),
+            a if self.is_alpha(a) => self.make_identifier(),
+            d if self.is_digit(d) => self.make_number(),
             _ => panic!("Invalid character"),
         };
 
@@ -172,19 +173,19 @@ impl<'a> Scanner<'a> {
             match c {
                 ' ' | '\r' | '\t' => {
                     self.advance();
-                },
+                }
                 '\n' => {
                     self.line += 1;
                     self.advance();
-                },
+                }
                 '/' => {
                     if self.peek_next() == '/' {
                         while self.peek() != '\n' && !self.is_at_end() {
-                             self.advance();
+                            self.advance();
                         }
                     }
                 }
-                _ => return
+                _ => return,
             }
         }
     }
@@ -199,7 +200,7 @@ impl<'a> Scanner<'a> {
         }
 
         return self.source[self.current + 1];
-      }
+    }
 
     fn make_string(&mut self) -> Token {
         while self.peek() != '"' && !self.is_at_end() {
@@ -237,5 +238,56 @@ impl<'a> Scanner<'a> {
         }
 
         return self.make_token(TokenType::Number);
+    }
+
+    fn is_alpha(&self, c: char) -> bool {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+    }
+
+    fn make_identifier(&mut self) -> Token {
+        while self.is_alpha(self.peek()) || self.is_digit(self.peek()) {
+            self.advance();
+        }
+
+        return self.make_token(self.identifier_type());
+    }
+
+    fn identifier_type(&self) -> TokenType {
+        let c = self.source[self.current];
+
+        return match c {
+            'a' => self.check_keyword("and", TokenType::And),
+            'c' => self.check_keyword("class", TokenType::Class),
+            'e' => self.check_keyword("else", TokenType::Else),
+            'f' if self.current - self.start > 1 => match self.source[self.start + 1] {
+                'a' => self.check_keyword("false", TokenType::False),
+                'o' => self.check_keyword("for", TokenType::For),
+                'u' => self.check_keyword("fun", TokenType::Fun),
+                _ => TokenType::Identifier,
+            },
+            'i' => self.check_keyword("if", TokenType::If),
+            'n' => self.check_keyword("nil", TokenType::Nil),
+            'o' => self.check_keyword("or", TokenType::Or),
+            'p' => self.check_keyword("print", TokenType::Print),
+            'r' => self.check_keyword("return", TokenType::Return),
+            's' => self.check_keyword("super", TokenType::Super),
+            't' if self.current - self.start > 1 => match self.source[self.start + 1] {
+                'h' => self.check_keyword("this", TokenType::This),
+                'r' => self.check_keyword("true", TokenType::True),
+                _ => TokenType::Identifier,
+            },
+            'v' => self.check_keyword("var", TokenType::Var),
+            'w' => self.check_keyword("while", TokenType::While),
+            _ => TokenType::Identifier,
+        };
+    }
+
+    fn check_keyword(&self, expected: &str, token_type: TokenType) -> TokenType {
+        let actual: String = self.source[self.start..=self.current].iter().collect();
+        if actual == expected {
+            return token_type;
+        }
+
+        return TokenType::Identifier;
     }
 }
