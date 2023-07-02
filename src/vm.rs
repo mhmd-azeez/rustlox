69c748d813820f82e5110825b012558145fda134
Muhammad Azeez <muhammad-azeez@outlook.com>
2023-07-02 15:44:02 +0300

    15.3: An arithmetic calculator

--- src/vm.rs
+++ src/vm.rs
@@ -4,6 +4,7 @@ use num_traits::FromPrimitive;
 pub struct VM {
     chunk: Chunk,
     ip: usize,
+    stack: Vec<Value>,
 }
 
 pub enum InterpretResult {
@@ -17,28 +18,51 @@ impl VM {
         return VM {
             chunk: chunk,
             ip: 0,
+            stack: vec![]
         };
     }
 
     pub fn intepret(mut self) -> InterpretResult {
         loop {
+
+            print!("          ");
+
+            for slot in self.stack.iter() {
+                print!("[ ");
+                debug::print_value(*slot);
+                print!(" ]");
+            }
+
+            println!();
+
             debug::disassemble_instruction(&self.chunk, self.ip);
 
             let instruction = OpCode::from_u8(self.read_byte());
             match instruction {
                 Some(value) => match value {
-                    OpCode::OpReturn => return InterpretResult::Ok,
+                    OpCode::OpReturn => {
+                        debug::print_value(self.pop());
+                        println!();
+                        return InterpretResult::Ok;
+                    },
                     OpCode::OpConstant => {
                         let index = self.read_byte();
                         let constant = self.read_constant(index);
-                        debug::print_value(constant);
-                        println!();
+                        self.push(constant);
                     }
                 },
                 None => return InterpretResult::RuntimeError,
             }
             
         }
+    }
+
+    pub fn push(&mut self, value: Value) {
+        self.stack.push(value);
+    }
+
+    pub fn pop(&mut self) -> Value {
+        return self.stack.pop().unwrap();
     }
 
     fn read_byte(&mut self) -> u8 {
